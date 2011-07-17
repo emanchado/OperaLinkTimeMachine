7b281e17db15932100da9138da2c0c268f5c9c6c
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2011-07-17 14:44:03 +0200

    Consider the path /favicon.ico static, even if there's nothing there yet

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ handle["^/start"] = requestHandlers.start;
 handle["^/get_request_token"] = requestHandlers.getRequestToken;
 handle["^/authorise"] = requestHandlers.authorise;
 handle["^/static/.+"] = "*static*";
+handle["^/favicon.ico"] = "*static*";
 
 server.start(router.route, handle);
