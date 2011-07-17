05c291094dd78e7ad315a4080de1d8beed98ed83
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2011-07-17 21:37:18 +0200

    Re-indent, clean-up logging

--- server.js
+++ server.js
@@ -2,26 +2,28 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
-  function onRequest(request, response) {
-    var postData = "";
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+    function onRequest(request, response) {
+        var postData = "";
+        var parsedUrl = url.parse(request.url);
+        var pathname = parsedUrl.pathname;
+        if (parsedUrl.query) {
+            console.log("Request for " + pathname + "?" + parsedUrl.query);
+        } else {
+            console.log("Request for " + pathname);
+        }
 
-    request.setEncoding("utf8");
+        request.setEncoding("utf8");
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+        });
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
+    }
 
-    request.addListener("data", function(postDataChunk) {
-      postData += postDataChunk;
-      console.log("Received POST data chunk '"+
-      postDataChunk + "'.");
-    });
-
-    request.addListener("end", function() {
-      route(handle, pathname, response, postData);
-    });
-  }
-
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+    var port = 8888;
+    http.createServer(onRequest).listen(port);
+    console.log("Server listening on http://localhost:" + port + "/");
 }
 
 exports.start = start;
